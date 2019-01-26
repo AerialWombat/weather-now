1133752dcd380169391a717aa751e9a6733076c9
AerialWombat <adrianzhan@gmail.com>
2019-01-26 14:10:28 -0500

    Added styling for header and footer

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,10 +4,15 @@ import styles from "./Header.module.scss";
 const Header = ({ unit }) => {
   return (
     <header>
-      <h1 className="title">Weather Now</h1>
-      <h3 className="subtitle">Where this is</h3>
-      <input className="search" type="text" placeholder="Location" />
-      <button>{unit === "SI" ? "F" : "C"}&#176;</button>
+      <h1 className={styles.title}>Weather Now</h1>
+      <h3 className={styles.subtitle}>New York</h3>
+      <form className={styles.searchBar}>
+        <input className={styles.search} type="text" placeholder="Location" />
+        <input className={styles.submit} type="submit" value="Search" />
+      </form>
+      <button className={styles.toggle}>
+        {unit === "SI" ? "F" : "C"}&#176;
+      </button>
     </header>
   );
 };
