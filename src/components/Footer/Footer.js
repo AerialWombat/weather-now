ed44196880677f15c02a933dffd4e4e8c6031fa7
AerialWombat <adrianzhan@gmail.com>
2019-02-02 21:36:18 -0500

    Adjusted styles for smaller heights. Optimized images

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -4,9 +4,9 @@ import styles from "./Footer.module.scss";
 const Footer = () => {
   return (
     <footer>
-      <a href="https://darksky.net/poweredby/" target="_blank">
+      <a href="https://darksky.net/poweredby/" target="_blank" rel="noopener noreferrer">
         <img
-          src={require("../../darksky.png")}
+          src={require("../../assets/Images/darksky.png")}
           alt="Powered by Dark Sky"
           className={styles.attribution}
         />{" "}
