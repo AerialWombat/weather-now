ee49fbcc8d31aedc8fee4c24eaa3e970b033f071
AerialWombat <adrianzhan@gmail.com>
2019-02-05 06:12:31 -0500

    Mobile viewport fix

--- src/App.js
+++ src/App.js
@@ -55,6 +55,11 @@ class App extends Component {
   }
 
   componentDidMount = () => {
+    // First we get the viewport height and we multiple it by 1% to get a value for a vh unit
+    let vh = window.innerHeight * 0.01;
+    // Then we set the value in the --vh custom property to the root of the document
+    document.documentElement.style.setProperty("--vh", `${vh}px`);
+
     if ("geolocation" in navigator) {
       navigator.geolocation.getCurrentPosition(position => {
         this.setState({
