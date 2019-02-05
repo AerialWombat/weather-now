c75ee051119feb697c9be8e805c9caeb632585f6
AerialWombat <adrianzhan@gmail.com>
2019-02-05 06:25:08 -0500

    Mobile viewport fix

--- src/App.js
+++ src/App.js
@@ -55,9 +55,8 @@ class App extends Component {
   }
 
   componentDidMount = () => {
-    // First we get the viewport height and we multiple it by 1% to get a value for a vh unit
+    // Set viewport height unit on mount to fix issues with mobile keyboard compressing styles
     let vh = window.innerHeight * 0.01;
-    // Then we set the value in the --vh custom property to the root of the document
     document.documentElement.style.setProperty("--vh", `${vh}px`);
 
     if ("geolocation" in navigator) {
