394cfa4a885fc5724b717859a6a596599be8f0d6
AerialWombat <adrianzhan@gmail.com>
2019-02-05 06:19:23 -0500

    Updates

---
+++ precache-manifest.f29b289e7aaf10e1eb362866895cd6b7.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = [
+  {
+    "revision": "2688ea8f255876fdf9ed823b9e04e1da",
+    "url": "/weather-now/static/media/wind.2688ea8f.jpg"
+  },
+  {
+    "revision": "cbb9ec6489e3fc4401d1",
+    "url": "/weather-now/static/css/main.2a926505.chunk.css"
+  },
+  {
+    "revision": "6a802cb1a8f1cd72f9bfbafd183d0951",
+    "url": "/weather-now/static/media/darksky.6a802cb1.png"
+  },
+  {
+    "revision": "084f6356faab8394ef04",
+    "url": "/weather-now/static/js/1.084f6356.chunk.js"
+  },
+  {
+    "revision": "d8c546a80bc8cd432ddf",
+    "url": "/weather-now/static/js/runtime~main.d8c546a8.js"
+  },
+  {
+    "revision": "45d431bf3b92976538d8c04d2a8da919",
+    "url": "/weather-now/static/media/night.45d431bf.jpg"
+  },
+  {
+    "revision": "5828dd5354d5014e43d024b9c6ca9773",
+    "url": "/weather-now/static/media/snow.5828dd53.jpg"
+  },
+  {
+    "revision": "85a3b63359772320208a1f1ed8cf01c1",
+    "url": "/weather-now/static/media/rainy.85a3b633.jpg"
+  },
+  {
+    "revision": "e415cc87f33ce09975af4ee31c3a9856",
+    "url": "/weather-now/static/media/sunny.e415cc87.jpg"
+  },
+  {
+    "revision": "cbb9ec6489e3fc4401d1",
+    "url": "/weather-now/static/js/main.cbb9ec64.chunk.js"
+  },
+  {
+    "revision": "e10f33236386e6979068391f9a7a0886",
+    "url": "/weather-now/static/media/cloud.e10f3323.jpg"
+  },
+  {
+    "revision": "ea591eb2a211915d33c4a996f0830ba5",
+    "url": "/weather-now/static/media/fog.ea591eb2.jpg"
+  },
+  {
+    "revision": "4b658767da6bd92ce2addb3ce512784d",
+    "url": "/weather-now/static/media/weathericons-regular-webfont.4b658767.eot"
+  },
+  {
+    "revision": "8cac70ebda3f23ce472110d9f21e8593",
+    "url": "/weather-now/static/media/weathericons-regular-webfont.8cac70eb.woff"
+  },
+  {
+    "revision": "4618f0de2a818e7ad3fe880e0b74d04a",
+    "url": "/weather-now/static/media/weathericons-regular-webfont.4618f0de.ttf"
+  },
+  {
+    "revision": "1cd48d78f06d33973d9d761d426e69bf",
+    "url": "/weather-now/static/media/weathericons-regular-webfont.1cd48d78.woff2"
+  },
+  {
+    "revision": "ecaf8b481729b18f6a8494d9f691cdae",
+    "url": "/weather-now/static/media/weathericons-regular-webfont.ecaf8b48.svg"
+  },
+  {
+    "revision": "084f6356faab8394ef04",
+    "url": "/weather-now/static/css/1.ef6b3e4c.chunk.css"
+  },
+  {
+    "revision": "ecf31afe6c7191a281fc5afc0ee78adf",
+    "url": "/weather-now/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/weather-now/precache-manifest.24a7bd914416243c8f5392f22b70ea71.js"
+  "/weather-now/precache-manifest.f29b289e7aaf10e1eb362866895cd6b7.js"
 );
 
 workbox.clientsClaim();
